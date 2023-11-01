977e62e3ba9a8d709d53fae62b971fc90a19bf58
조수빈 <71023617+chosoobin37@users.noreply.github.com>
2023-11-01 16:50:47 +0900

    Delete chatbot directory

--- chatbot/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class LegalQAFinal(models.Model):
-    question = models.CharField(max_length=500)
-    answer = models.TextField()
-    law = models.CharField(max_length=200, null=True, blank=True)
-    prec = models.CharField(max_length=200, null=True, blank=True)
-    embedding = models.TextField(null=True, blank=True)
